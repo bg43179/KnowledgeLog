7a8b6e944d4c039dc5fe3782e8de331d014ca9cf
Andrew <b00602013@ntu.edu.tw>
2019-04-23 00:10:10 -0500

    create: transform data from tsv to nt

---
+++ src/utils/clean.py
@@ -0,0 +1,10 @@
+op = open('../data/dbpedia.3.8.nt', "w")
+
+with open('../data/dbpedia.3.8.tsv') as fp: 
+	for line in fp:
+		line = line.replace("	", "")
+		line = line.replace("\n", ".")
+		op.write(line + "\n")
+		print(".", end=' ')
+
+op.close()
