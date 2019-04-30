963cc7cd4db66b04d276681b8b2a1edc012ed96b
Paulluh <paul841029@gmail.com>
2019-04-30 00:33:21 -0500

    minor refactor and experiment

--- src/knowledge.py
+++ src/knowledge.py
@@ -5,180 +5,177 @@ from tree import rule_selector
 from node import RuleNode, trim
 from pyDatalog import pyDatalog
 from request import pull_from_fuseki
+import loguru
+import collections
 
 
-def cache(response, sub, predicate, obj, predicate_map = {}):
-	"""
-		Method for pulling data form fuseki, support 4 different requests body
+def cache(response, sub, predicate, obj, predicate_map={}):
+    """
+        Method for pulling data form fuseki, support 4 different requests body
 
-	Args:
-		response: json(dict) return from fuseki server
-	    subject: subject
-	    predicate: predicate
-	    obj: object 
-	    predicate: Use for checking if the relation(predicate) talbe already exist
-	"""
-	# first create terms for all the predicates
-	count = 0
-	# add an instance in this format (+prdicate(subject, object))
-	for index, instance in enumerate(response['results']['bindings']):	
-		
-		current_pred = predicate
-		item = {}
-		
-		for key, value in instance.items():
-			
-			# create a new relation table if the given predicate is not exist, can be removed in our case
-			# if key == predicate:
-			# 	if key not in predicate_map:
-			# 		pyDatalog.create_terms(value["value"].split(":")[1])
-			# 		predicate_map[value["value"].split(":")[1]]= 1
-			# 	current_pred = value["value"].split(":")[1]
+    Args:
+        response: json(dict) return from fuseki server
+        subject: subject
+        predicate: predicate
+        obj: object
+        predicate: Use for checking if the relation(predicate) talbe already exist
+    """
+    # first create terms for all the predicates
+    count = 0
+    # add an instance in this format (+prdicate(subject, object))
+    for index, instance in enumerate(response['results']['bindings']):
 
-			if key == sub:
-				item[sub] = value["value"].split(":")[1]
-				
-			if key == obj:
-				item[obj] = value["value"].split(":")[1]
-		
-		# add the fact
-		pyDatalog.assert_fact(current_pred, item[sub], item[obj])
-		count += 1
+        current_pred = predicate
+        item = {}
 
-	print(count)
+        for key, value in instance.items():
+
+            # create a new relation table if the given predicate is not exist, can be removed in our case
+            # if key == predicate:
+            # 	if key not in predicate_map:
+            # 		pyDatalog.create_terms(value["value"].split(":")[1])
+            # 		predicate_map[value["value"].split(":")[1]]= 1
+            # 	current_pred = value["value"].split(":")[1]
+
+            if key == sub:
+                item[sub] = value["value"].split(":")[1]
+
+            if key == obj:
+                item[obj] = value["value"].split(":")[1]
+
+        # add the fact
+        pyDatalog.assert_fact(current_pred, item[sub], item[obj])
+        count += 1
+
+    loguru.logger.debug(count)
+
 
 def partition_cache(node, predicate_map, no_of_partition=5):
-	
-	"""
-		This method can load all the relation in one rule into memory. 
-		Divide all the relation into 5 different partition, each represent different probability
-	"""
-	table = []
+    """
+        This method can load all the relation in one rule into memory.
+        Divide all the relation into 5 different partition, each represent different probability
+    """
+    table = []
 
-	for predicate in node.raw_set:
-		
-		predicates = []	
-		
-		for num in range(no_of_partition):
-			name = trim(predicate ,1, "r") + "__prob__" + str(num+1)
-			predicates.append(name)
-			
-			if name not in predicate_map:
-				pyDatalog.create_terms(name)
-				predicate_map[name] = 1
+    for predicate in node.raw_set:
 
-				if (num+1) == no_of_partition:
-				# if data haven't been loaded into DB, add it from fuseki 
-					sub, obj= "subject", "object"
-					predicate_map[predicates[-1]] = 1
+        predicates = []
 
-					# TODO: modify the string passed in 
-					print(predicate)
-					response = pull_from_fuseki(sub, predicate, obj, 2)
-					cache(response, sub, predicates[-1], obj, predicate_map)
+        for num in range(no_of_partition):
+            name = trim(predicate, 1, "r") + "__prob__" + str(num + 1)
+            predicates.append(name)
 
-		table.append(predicates[:])
+            if name not in predicate_map:
+                pyDatalog.create_terms(name)
+                predicate_map[name] = 1
 
+                if (num + 1) == no_of_partition:
+                    # if data haven't been loaded into DB, add it from fuseki
+                    sub, obj = "subject", "object"
+                    predicate_map[predicates[-1]] = 1
 
-	# Join two relations(or one relation)
-	# for i in range(len(table)):
-		# for j in range(len(table[0])):
+                    # TODO: modify the string passed in
+                    loguru.logger.debug(predicate)
+                    response = pull_from_fuseki(sub, predicate, obj, 2)
+                    cache(response, sub, predicates[-1], obj, predicate_map)
+
+        table.append(predicates[:])
 
 
-	# print(table)
-	# print(predicate_map)
+# Join two relations(or one relation)
+# for i in range(len(table)):
+# for j in range(len(table[0])):
+
+
+# loguru.logger.debug(table)
+# loguru.logger.debug(predicate_map)
 
 
 def partition_loader():
-	"""
-		Method for loading rule and load data into memory at once(cache)
-	"""
+    """
+        Method for loading rule and load data into memory at once(cache)
+    """
 
-	rules, relations = rule_selector("<dbo:author>", 2)
-	knowledge(rules)
+    rules, relations = rule_selector("<dbo:author>", 2)
+    knowledge(rules)
 
 
-def loader(): 
-	"""
-		Method for loading rule and load data into memory at once(cache)
-	"""
+def loader():
+    """
+        Method for loading rule and load data into memory at once(cache)
+    """
 
-	# load rule and generate a list of relations
-	rules, relations = rule_selector("<dbo:author>", 1)
-	predicate_map = {}
+    # load rule and generate a list of relations
+    rules, relations = rule_selector("<dbo:author>", 1)
+    predicate_map = {}
 
-	for predicate in relations:
-		sub, obj= "subject", "object"
+    for predicate in relations:
+        sub, obj = "subject", "object"
 
-		pred = trim(predicate, 1, "r")
-		print(pred)
+        pred = trim(predicate, 1, "r")
+        loguru.logger.debug(pred)
 
-		if pred not in predicate_map:
-			pyDatalog.create_terms(pred)
-			predicate_map[pred] = 1
+        if pred not in predicate_map:
+            pyDatalog.create_terms(pred)
+            predicate_map[pred] = 1
 
-		response = pull_from_fuseki(sub, predicate, obj, 2)
-		cache(response, sub, pred, obj, predicate_map)
+        response = pull_from_fuseki(sub, predicate, obj, 2)
+        cache(response, sub, pred, obj, predicate_map)
+
+def eval(data, rule):
+    pass
+
 
 
 def knowledge(rules):
-	
-	predicate_map = {}
-	rule_map = {}
-	idb_set = set()
-	
+    def get_next_rule(rules):
+        rule_map = {}
+        idb_set = set()
+        head_counter = collections.defaultdict(lambda: 0)
 
-	# iterate thru all rules and find the idb and edb
-	for index, rule in enumerate(rules):
-	
-		# TODO: fix input
+        for index, rule in enumerate(rules):
+            # TODO: fix input
 
-		node = RuleNode(rule[0], rule[1])
-		idb_set.add( node.left )
+            node = RuleNode(rule[0], rule[1])
+            idb_set.add(node.left)
+            head_counter[node.left] += 1
 
-		rule_map[str(index)] = node
+            rule_map[str(index)] = node
 
-	# while rule_map is not empty
-	# print("\n".join(n.rule for n in rule_map.values()))
+        while rule_map:
+            to_remove = set()
 
-	while rule_map:
-		to_remove = set()
+            for key in list(rule_map.keys()):
+                node = rule_map[key]
+                # loguru.logger.debug(node.right_set)
+                if not idb_set.intersection(node.right_set):
+                    yield node
 
-		for key in list(rule_map.keys()):
-			node = rule_map[key]
-			# print(node.right_set)
-			if not idb_set.intersection(node.right_set):
-				
-				partition_cache(node, predicate_map)
-				
-				to_remove.add(node.left)
-				
-				del rule_map[key]
+                    head_counter[node.left] -= 1
 
-		print("Before", idb_set)
-						
-		idb_set = idb_set.difference(to_remove)
-		print("")
-		print("\n".join(n.rule for n in rule_map.values()))
-		
-		print("")
-		# print(to_remove)
-		print("After", idb_set)
-		print("")
+                    if head_counter[node.left] == 0:
+                        to_remove.add(node.left)
 
+                    del rule_map[key]
+
+            idb_set = idb_set.difference(to_remove)
+
+    for rule in get_next_rule(rules):
+        loguru.logger.info(rule)
 
 
 if __name__ == "__main__":
-	
+    # sub, pred, obj= "subject", "predicate", "object"
+    # response = pull_from_fuseki(sub, pred, obj, 0);
+    # cache(sub, pred, obj)
 
-	# sub, pred, obj= "subject", "predicate", "object"
-	# response = pull_from_fuseki(sub, pred, obj, 0);
-	# cache(sub, pred, obj)
-	
-	# loader()
-	partition_loader()
-	pyDatalog.create_terms("X", "Y", "Z")
-	# print(pyDatalog.ask("influenced" + '(X, Y)'))
+    # loader()
+    # partition_loader()
+    pyDatalog.create_terms("r,x,y")
+    # loguru.logger.debug(pyDatalog.ask("influenced" + '(X, Y)'))
 
-	# print(pyDatalog.ask('author' + '(X, "George_Orwell")'))
-
+    # loguru.logger.debug(pyDatalog.ask('author' + '(X, "George_Orwell")'))
+    pyDatalog.load(
+        "+ r('a','b')"
+    )
+    print(pyDatalog.ask("r(x,y)"))
--- src/node.py
+++ src/node.py
@@ -1,37 +1,38 @@
 class RuleNode():
-	def __init__(self, rule, conf=None):
-		
-		self.rule = rule
-		rule = rule.split("<=")
-		
-		right = rule[1].split("&")
+    def __init__(self, rule, conf=None):
+        self.rule = rule
+        rule = rule.split("<=")
 
-		self.right_set =  set([trim(right[0], 1, "r")]) if len(right) == 1 else set([trim(right[0], 1, "r"), trim(right[1], 1, "r")])
-		self.raw_set = set([trim(right[0], 2,), trim(right[1], 2,)]) if len(right) == 2 else set([trim(right[0], 2)])
-		# self.raw_right_set = set([trim(right[0], 2,), trim(right[1], 2,)]) if len(right) == 2 else set([trim(right[0], 2)])
-		# self.raw_all_set = set([rule[0], right[0], right[1]]) if len(right) == 2 else set([rule[0], right[0]])
-		
-		self.right = rule[1].strip()
-		self.left = trim(rule[0], 1, "r")
-		self.conf = conf
+        right = rule[1].split("&")
+
+        self.right_set = set([trim(right[0], 1, "r")]) if len(right) == 1 else set(
+            [trim(right[0], 1, "r"), trim(right[1], 1, "r")])
+        self.raw_set = set([trim(right[0], 2, ), trim(right[1], 2, )]) if len(right) == 2 else set([trim(right[0], 2)])
+        # self.raw_right_set = set([trim(right[0], 2,), trim(right[1], 2,)]) if len(right) == 2 else set([trim(right[0], 2)])
+        # self.raw_all_set = set([rule[0], right[0], right[1]]) if len(right) == 2 else set([rule[0], right[0]])
+
+        self.right = rule[1].strip()
+        self.left = trim(rule[0], 1, "r")
+        self.conf = conf
+
+    def __repr__(self):
+        return repr((self.rule, self.conf))
+
 
 def trim(string, option=1, replace="f"):
-	
-	"""
-		string: predicate i.e."<dbo:launchSite>(a, b)
-	"""
+    """
+        string: predicate i.e."<dbo:launchSite>(a, b)
+    """
 
-	# get rid of everything => launchSite
-	if option == 1: 
-		string = string.split(":")[1].strip().split(">")[0]
-	# get rid of parentheses => <dbo:launchSite>
-	elif option == 2:
-		string = string.split("(")[0]
+    # get rid of everything => launchSite
+    if option == 1:
+        string = string.split(":")[1].strip().split(">")[0]
+    # get rid of parentheses => <dbo:launchSite>
+    elif option == 2:
+        string = string.split("(")[0]
 
+    if replace == "r":
+        string = string.replace("/", "")
+        string = string.replace(".", "")
 
-	if replace == "r":
-		string = string.replace("/", "")
-		string = string.replace(".", "")
-
-
-	return string.strip()
+    return string.strip()
