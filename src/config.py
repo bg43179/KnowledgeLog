1761d8941184673642b6ec343760d1dbf1300e6c
Andrew <b00602013@ntu.edu.tw>
2019-05-02 09:47:35 -0500

    update: add new parameters to config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
-num_split = 5
+num_split = 10
 rule_depth = 2
-query_relation = "<dbo:author>"
+request_type = 2
+score_threshold = 0.4
+query_relation = "<dbo:notableWork>"
+query_relation_name = "notableWork"
